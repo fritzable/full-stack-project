2f2eee7055949f81e32dddb8827534a8b97409ba
fritzable <fritzzillad@aol.com>
2019-03-04 16:19:27 -0500

    Update command add for Patients, Ingredients
    Add routes, controller logic for PATCH request to Patients, Ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,17 @@ Rails.application.routes.draw do
   get '/books' => 'books#index'
   get '/books/:id' => 'books#show'
   post '/books' => 'books#create'
+  patch '/books/:id' => 'books#update'
 
   # Patients resource routes
   get '/patients' => 'patients#index'
   get '/patients/:id' => 'patients#show'
   post '/patients' => 'patients#create'
+  patch '/patients/:id' => 'patients#update'
 
   # Ingredients resource routes
   get '/ingredients' => 'ingredients#index'
   get '/ingredients/:id' => 'ingredients#show'
   post '/ingredients' => 'ingredients#create'
+  patch '/ingredients/:id' => 'ingredients#update'
 end
